3507137e023467a4dc9fba81d16728fb5750720e
austin <galaky2005@gmail.com>
2022-12-26 11:37:20 -0500

    added references to gomez and rusymbols with the goal of using them for constructing calculations dynamically and then solving systems of equations with gomez;

---
+++ src/symbolic_nonlinear_system_solver.rs
@@ -0,0 +1,7 @@
+
+
+// Purpose:
+//      To consume a cell group dependency manager
+//      Iterate over all possible/valid dependencies
+//      Construct symbolic equation via rusymbol
+//      Solve symbolic equation via gomez
